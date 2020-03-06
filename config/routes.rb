a276e9500534cf198123ef69015585f3961bfa84
kakeru <okinawan.kkr0713@icloud.com>
2020-03-06 18:54:18 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'post/create'
-  get 'post/index'
-  get 'post/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :post
+  root to:'posts#index'
 end
